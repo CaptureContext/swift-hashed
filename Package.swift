497c28164a197189ec93f213b3c84f31bae8ea6b
maximkrouk <id.maximkrouk@gmail.com>
2026-09-03 14:58:32 +0200

    chore: update dependencies

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/capturecontext/swift-equated.git",
-			.upToNextMinor(from: "0.0.5")
+			.upToNextMinor(from: "0.0.6")
 		),
 	],
 	targets: [
